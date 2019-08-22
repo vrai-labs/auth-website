463b624724e96a9767f4c4bcc8658130e6176668
Rishabh Poddar <rishabh.poddar@gmail.com>
2019-08-22 14:13:36 +0000

    [skip ci]
    Deploy website version based on 4f065a8452b4665eb7555ec29620642b42709dbf

--- scripts/homeButtonListener.js
+++ scripts/homeButtonListener.js
@@ -282,6 +282,6 @@ document.addEventListener("DOMContentLoaded", () => {
     gtag('config', 'UA-143540696-1');
 
     let body = document.getElementsByTagName("body")[0];
-    addChat();
+    // addChat();
     body.style.display = "block";
 });
