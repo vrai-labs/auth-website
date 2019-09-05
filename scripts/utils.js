18662d06568d023ba1d04b906bc953d007fc24e1
Rishabh Poddar <rishabh.poddar@gmail.com>
2019-09-05 11:22:57 +0000

    [skip ci]
    Deploy website version based on 823f6f9b4679208c2fa8884dd105fb2c37e15c45

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,4 +1,4 @@
-let latestVersion = "4.3.X"; /*THIS COMMENT IS USED TO REPLACE THE LATEST VERSION*/
+let latestVersion = "3.2.X"; /*THIS COMMENT IS USED TO REPLACE THE LATEST VERSION*/
 
 function getGithubUrl() {
     return "https://github.com/supertokens/supertokens-website";
